5f024583230032904c1b9e2d931e0a682b3c061b
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-03-11 19:37:13 +0000

    Fixed order of hypersphere dimentions

--- cwc/data/reject.py
+++ cwc/data/reject.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python
-
 import numpy
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
+
+def hypercube_distribution(size, dimensions):
+    return numpy.random.rand(dimensions, num_reject)*2-1
 
 def hypersphere_distribution(size, dimensions):
     '''
@@ -10,11 +11,11 @@ def hypersphere_distribution(size, dimensions):
     http://math.stackexchange.com/questions/87230/picking-random-points-in-the-volume-of-sphere-with-uniform-probability
     '''
     U = numpy.random.rand(size)
-    X = numpy.random.normal(size=(dimensions, size))
+    X = numpy.random.normal(size=(size, dimensions))
 
-    sphere1 = numpy.power(U, 1.0/dimensions)
+    sphere1 = numpy.power(U, 1.0/dimensions).reshape((-1,1))
     sphere2 = sphere1*X
-    sphere3 = sphere2/numpy.sqrt(numpy.sum(X**2.0, axis=0))
+    sphere3 = sphere2/numpy.sqrt(numpy.sum(X**2.0, axis=1)).reshape((-1,1))
 
     return sphere3
 
@@ -23,12 +24,13 @@ def hypersphere_boundary_distribution(size, dimensions):
 
     radius = numpy.sqrt((normal_deviates**2).sum(axis=0))
     points = normal_deviates/radius
+    return points
 
 def create_reject_data(X, proportion, method):
     num_reject = X.shape[0]*proportion
     dimensions = X.shape[1]
     if method == 'uniform_hcube':
-        return numpy.random.rand(dimensions, num_reject)
+        return hypercube_distribution(num_reject, dimensions)
     elif method == 'uniform_hsphere':
         return hypershpere_distribution(num_reject, dimensions)
 
@@ -38,12 +40,9 @@ def test_hypersphere():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
-    ax.scatter(x[0,:], x[1,:], x[2,:])
+    ax.scatter(x[:,0], x[:,1], x[:,2])
 
     plt.show()
 
 if __name__ == '__main__':
     test_hypersphere()
-
-
-
