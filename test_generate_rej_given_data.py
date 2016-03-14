b4c19ba62d91a9ea8de547536e7d5c1603cd905c
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-03-14 18:22:20 +0000

    Added example that generates reject points in a 2 dimensional hypersphere given random data sampled from two gaussians

--- test_generate_rej_given_data.py
+++ test_generate_rej_given_data.py
@@ -9,8 +9,8 @@ from cwc.synthetic_data import toy_examples
 from cwc.synthetic_data import reject
 
 
-if __name__ == '__main__':
-    samples = [1000, 1000]
+def test_rej_data():
+    samples = [500, 500]
     x, y = toy_examples.generate_gaussians(means=[[3,3,3],[5,5,5]],
                                   covs=[[[2,1,0],[1,1,0],[0,0,1]],
                                         [[1,0,0],[0,1,1],[0,1,2]]],
@@ -42,4 +42,27 @@ if __name__ == '__main__':
     # TODO compute the value of r before creating the hypersphere
     r *= numpy.sqrt(1.1/(numpy.mean(r**2)))
 
-    plt.scatter(r[:,0], r[:,1], c='m')
+    plt.scatter(r[:,0], r[:,1], c='y')
+
+    r_transform = pca.inverse_transform(r)
+
+    ax.scatter(r_transform[:,0], r_transform[:,1], r_transform[:,2], c='y')
+
+def test_rej_data_given_x():
+    samples = [500, 500]
+    x, y = toy_examples.generate_gaussians(means=[[3,3,3],[5,5,5]],
+                                  covs=[[[2,1,0],[1,1,0],[0,0,1]],
+                                        [[1,0,0],[0,1,1],[0,1,2]]],
+                                  samples=samples)
+
+    fig = plt.figure('data')
+    ax = fig.add_subplot(111, projection='3d')
+    for k, c in [(0, 'r'), (1, 'b')]:
+        index = (y == k)
+        ax.scatter(x[index,0], x[index,1], x[index,2], c=c)
+
+    r = reject.create_reject_data(x, proportion=1, method='uniform_hsphere')
+    ax.scatter(r[:,0], r[:,1], r[:,2], c='y')
+
+if __name__ == '__main__':
+    test_rej_data_given_x()
