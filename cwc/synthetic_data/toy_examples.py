92ac47d96f3daba5bb1de0c8ddca4fe897478872
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-03-14 18:21:25 +0000

    Added comments

--- cwc/synthetic_data/toy_examples.py
+++ cwc/synthetic_data/toy_examples.py
@@ -4,7 +4,20 @@ import matplotlib.pyplot as plt
 
 
 def generate_gaussians(means=[[0,0]], covs=[[[1,0],[0,1]]], samples=[1]):
-    print('means shape = {} len = {}'.format(numpy.shape(means), len(means)))
+    """This function generates N Gaussian distributions with the specified
+        mean, covariance and number of samples per Gaussian.
+
+    Args:
+        means ([[float]]): The D-dimensional means for each Gaussian.
+        covs ([[[float]]]): The D-dimensional covariance matrix for each
+            Gaussian.
+        samples ([int]): The number of samples for each Gaussian.
+
+    Returns:
+        X ([[float]]): A matrix of size sum(samples) times D.
+        Y ([int]): A vector of size sum(samples) indicating the Gaussian
+            distribution where every X belongs.
+    """
     X = numpy.empty(shape=(sum(samples),numpy.shape(means)[1]))
     Y = numpy.empty(shape=(sum(samples)))
     cumsum = 0
@@ -22,6 +35,8 @@ def generate_gaussians(means=[[0,0]], covs=[[[1,0],[0,1]]], samples=[1]):
     return X, Y
 
 def test_generate_gaussians():
+    """This function generates two Gaussian distributions and plots them.
+    """
     x, y = generate_gaussians(means=[[0,0,0],[1,1,1]],
                                   covs=[[[2,1,0],[1,1,0],[0,0,1]],
                                         [[1,0,0],[0,1,1],[0,1,2]]],
