cbf49f4e116a5c7ebbccf7772b5e94d6739fd5d7
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-03-15 17:41:10 +0000

    Added option to scale the size of the reject data hyper-shape. Reused the computation of the radius of the hypersphere to the hypercube case.

--- cwc/synthetic_data/reject.py
+++ cwc/synthetic_data/reject.py
@@ -72,7 +72,8 @@ def hypersphere_boundary_distribution(size, dimensions):
 
 
 def create_reject_data(X, proportion, method, pca=False, pca_components=0,
-                       pca_variance=0, hshape_cov=0, hshape_prop_in=0):
+                       pca_variance=0, hshape_cov=0, hshape_prop_in=0,
+                       hshape_multiplier=1):
     """Generates random samples with the specified distribution.
 
     This function generates data points with the specified distribution from
@@ -86,24 +87,26 @@ def create_reject_data(X, proportion, method, pca=False, pca_components=0,
             - 'uniform_hsphere': Uniform hypersphere
         PCA (bool): Indicates if the reject points should be generated in a
             lower dimensionality space.
-        pca_components (int): e.g. 2 Number of dimensions of the hyper-shape in the
-            lower dimensionality space (this option and pca_variance are
+        pca_components (int): e.g. 2 Number of dimensions of the hyper-shape in
+            the lower dimensionality space (this option and pca_variance are
             mutually exclusive if both options are set at the same time the
             function will raise an Exception).
-        pca_variance (float): e.g. 0.99 Proportion of variance (0.0,1.0) to keep in the
-            lower dimensionality space (this option and pca_components are
-            mutually exclusive if both options are set at the same time the
-            function will raise an Exception).
+        pca_variance (float): e.g. 0.99 Proportion of variance (0.0,1.0) to
+            keep in the lower dimensionality space (this option and pca_
+            components are mutually exclusive if both options are set at the
+            same time the function will raise an Exception).
         hshape_cov (float): e.g. 1.1
         hshape_prop_in (float): Proportion of data points that need to be
             inside of the hyper-shape.
+        hshape_multiplier (float): Increases or decreases the distance to the
+            boundary of the hyper-shape.
 
     Returns:
         ([[float]]): A matrix of size (size x dimensions)
     """
     if pca == True:
         if pca_components and pca_variance:
-            raise Exeption('Options pca_components and pca_variance are'
+            raise Exception('Options pca_components and pca_variance are'
                            'mutually exclusive')
         elif pca_components:
             dimensions = pca_components
@@ -116,29 +119,35 @@ def create_reject_data(X, proportion, method, pca=False, pca_components=0,
             dimensions = numpy.argmax(pc.explained_variance_ratio_.cumsum() >=
                                       pca_variance)+1
         else:
-            raise Exeption('If PCA is selected pca_components or pca_variance '
+            raise Exception('If PCA is selected pca_components or pca_variance '
                            'need to be specified')
     else:
         dimensions = X.shape[1]
 
     num_reject = X.shape[0]*proportion
+
+    if hshape_prop_in and hshape_cov:
+        raise Exception('Options hshape_prop_in and hshape_cov are'
+                       'mutually exclusive')
+
+    if hshape_prop_in:
+        X_transform = pc.transform(X)
+        distances = numpy.linalg.norm(X_transform, axis=1)
+        distances.sort()
+        radius = distances[int(X.shape[0]*hshape_prop_in)-1]
+    else:
+        radius = 1
+
+    radius *= hshape_multiplier
+
     if method == 'uniform_hcube':
-        r = hypercube_distribution(num_reject, dimensions)
+        r = hypercube_distribution(num_reject, dimensions, a=-radius/2,
+                b=radius/2)
+
         if hshape_cov:
+            # FIXME check the equation of the variance for hypercube
             r *= numpy.sqrt(hshape_cov/(numpy.mean(r**2)))
     elif method == 'uniform_hsphere':
-        if hshape_prop_in and hshape_cov:
-            raise Exeption('Options hshape_prop_in and hshape_cov are'
-                           'mutually exclusive')
-
-        if hshape_prop_in:
-            X_transform = pc.transform(X)
-            distances = numpy.linalg.norm(X_transform, axis=1)
-            distances.sort()
-            radius = distances[int(X.shape[0]*hshape_prop_in)-1]
-        else:
-            radius = 1
-
         r = hypersphere_distribution(num_reject, dimensions,
                                             radius=radius)
         # TODO compute the value of r before creating the hypersphere
