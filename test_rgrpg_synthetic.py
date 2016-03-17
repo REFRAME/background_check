c46d557135a1ab5f3cb46bff88dc7ecaffb3bfbb
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-03-17 19:05:07 +0000

    Changed the classifiers of step 1 and step 2 for Decision Trees. Also print the accuracy of both classifiers and the performance of a classifier that always predicts the majority class

--- test_rgrpg_synthetic.py
+++ test_rgrpg_synthetic.py
@@ -1,9 +1,12 @@
+from __future__ import division
+
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 plt.ion()
 
 from sklearn.mixture import GMM
+from sklearn import svm
 from sklearn import tree
 
 from cwc.synthetic_data import toy_examples
@@ -11,8 +14,8 @@ from cwc.synthetic_data import reject
 from cwc.evaluation.rgrpg import RGRPG
 
 def generate_data():
-    samples = [700,         # Class 1
-               400]         # Class 2
+    samples = [500,         # Class 1
+               200]         # Class 2
     means = [[3,3,3],       # Class 1
              [3,5,5]]       # Class 2
     covs = [[[2,1,0],       # Class 1
@@ -27,26 +30,6 @@ def generate_data():
                                            samples=samples)
     return x, y
 
-
-def generate_reject_data(x):
-    # Options for reject data generation
-    method = 'uniform_hsphere'
-    proportion = 0.9
-    hshape_cov = 0
-    hshape_prop_in = 0.99
-    hshape_multiplier = 1.5
-    pca = True
-    pca_components = 0
-    pca_variance = 0.9
-    r = reject.create_reject_data(x, proportion=proportion,
-                                  method=method, pca=pca,
-                                  pca_variance=pca_variance,
-                                  pca_components=pca_components,
-                                  hshape_cov=hshape_cov,
-                                  hshape_prop_in=hshape_prop_in,
-                                  hshape_multiplier=hshape_multiplier)
-    return r
-
 def plot_data_and_reject(x,y,r):
     # Options for plotting
     colors = ['r', 'b', 'y']    # One color per class + reject
@@ -57,37 +40,36 @@ def plot_data_and_reject(x,y,r):
     for k, c in enumerate(colors[:-1]):
         index = (y == k)
         ax.scatter(x[index,0], x[index,1], x[index,2], c=c)
-    ax.scatter(r[:,0], r[:,1], r[:,2], c=colors[-1])
+    ax.scatter(r[:,0], r[:,1], r[:,2], marker='x', c=colors[-1])
 
 
 def train_reject_model(x,r):
-    # TODO compare with a reject model that is not generative
-    n_components=10
-    covariance_type='diag'
-    random_state=42
-    thresh=None
-    tol=0.001
-    min_covar=0.001
-    n_iter=100
-    n_init=1
-    params='wmc'
-    init_params='wmc'
-    verbose=0
-    model_rej = GMM(n_components=n_components, covariance_type=covariance_type,
-                    random_state=random_state, thresh=thresh, tol=tol,
-                    min_covar=min_covar, n_iter=n_iter, n_init=n_init,
-                    params=params, init_params=init_params, verbose=verbose)
-    model_rej.fit(x)
+    """Train a classifier of training points
+
+    Returns a classifier that predicts high probability values for training
+    points and low probability values for reject points.
+    """
+    #model_rej = svm.SVC(probability=True)
+    model_rej = tree.DecisionTreeClassifier(max_depth=8)
+
+    xr = np.vstack((x,r))
+    y = np.hstack((np.ones(np.alen(x)), np.zeros(np.alen(r)))).T
+    model_rej.fit(xr, y)
+
     return model_rej
 
 def train_classifier_model(x,y):
+    #model_clas = svm.SVC(probability=True)
     model_clas = tree.DecisionTreeClassifier(max_depth=2)
     model_clas = model_clas.fit(x,y)
     return model_clas
 
 if __name__ == "__main__":
     x,y = generate_data()
-    r = generate_reject_data(x)
+    r = reject.create_reject_data(x, proportion=1, method='uniform_hsphere',
+                                  pca=True, pca_variance=0.9, pca_components=0,
+                                  hshape_cov=0, hshape_prop_in=0.99,
+                                  hshape_multiplier=2)
     plot_data_and_reject(x,y,r)
 
     # Classifier of reject data
@@ -97,16 +79,40 @@ if __name__ == "__main__":
     model_clas = train_classifier_model(x,y)
 
     # Get scores
-    step1_reject_scores = model_rej.predict_proba(r).max(axis=1)
-    step1_training_scores = model_rej.predict_proba(x).max(axis=1)
+    step1_reject_scores = model_rej.predict_proba(r)[:,1]
+    step1_training_scores = model_rej.predict_proba(x)[:,1]
     step2_training_scores = model_clas.predict_proba(x)[:,0]
     training_labels = y
 
+    # Show scores
+    fig = plt.figure('scores')
+    fig.clf()
+    ax = fig.add_subplot(111)
+    ax.set_title('Scores')
+    ax.hist([step1_reject_scores, step1_training_scores,
+             step2_training_scores])
+    ax.legend(['step1_reject', 'step1_training', 'step2_class1'],
+              loc='upper center')
+
     # Evaluate models
+    # Accuracy
+    print("Step1 Accuracy = {} (prior = {})".format(
+        (np.sum(step1_reject_scores < 0.5) +
+         np.sum(step1_training_scores >= 0.5))/(np.alen(x)+np.alen(r)),
+         np.max([np.alen(x),np.alen(r)])/(np.alen(x)+np.alen(r))))
+
+    print("Step2 Accuracy = {} (prior = {})".format(
+        np.mean((step2_training_scores < 0.5) == y),
+        np.max([1-np.mean(y), np.mean(y)])))
+
+    # Volume under the PRG-ROC surface
     rgrpg = RGRPG(step1_reject_scores, step1_training_scores,
                   step2_training_scores, training_labels)
 
-    print rgrpg.calculate_volume()
-    plt.figure()
-    rgrpg.plot_rgrpg_2d()
-    rgrpg.plot_rgrpg_3d()
+    print("Volume = {}".format(rgrpg.calculate_volume()))
+    fig = plt.figure('rgrpg_2d')
+    fig.clf()
+    rgrpg.plot_rgrpg_2d(fig)
+    fig = plt.figure('rgrpg_3d')
+    fig.clf()
+    rgrpg.plot_rgrpg_3d(fig)
