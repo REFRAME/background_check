67791f6348493293411eb5608728db40f36b8eab
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-05-10 16:43:52 +0100

    Added pandas DataFrame to store all the intermediate results, sumarize and export into csv and LaTeX

--- hempstalk_results.py
+++ hempstalk_results.py
@@ -14,6 +14,7 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.tree import DecisionTreeClassifier
 from cwc.synthetic_data.datasets import MLData
 
+import pandas as pd
 
 mldata = MLData()
 
@@ -59,15 +60,6 @@ class MyMultivariateNormal(object):
         return self.norm_const * result
 
 
-def separate_sets(x, y, test_fold_id, test_folds):
-    x_test = x[test_folds == test_fold_id, :]
-    y_test = y[test_folds == test_fold_id]
-
-    x_train = x[test_folds != test_fold_id, :]
-    y_train = y[test_folds != test_fold_id]
-    return [x_train, y_train, x_test, y_test]
-
-
 class MultivariateNormal(object):
     def __init__(self, allow_singular=True, covariance_type='diag'):
         self.allow_singular = allow_singular
@@ -85,16 +77,52 @@ class MultivariateNormal(object):
     def score(self,x):
         return self.model.pdf(x)
 
+
+def export_results(table):
+    def mean_format(x):
+        return "%1.3f" % x
+
+    def std_format(x):
+        return '$\pm$%1.2f' % x
+
+    def dictionary_of_formats(table):
+        functions = table.columns.levels[0]
+        models = table.columns.levels[1]
+        dict_format = {}
+        for function in functions:
+            for model in models:
+                if function == 'mean':
+                    dict_format[(function, model)] = mean_format
+                elif function == 'std':
+                    dict_format[(function, model)] = std_format
+        return dict_format
+
+    table.to_csv('final_table.csv')
+
+    dict_format = dictionary_of_formats(table)
+    table.to_latex('final_table.tex', formatters=dict_format, escape=False)
+
+
+def separate_sets(x, y, test_fold_id, test_folds):
+    x_test = x[test_folds == test_fold_id, :]
+    y_test = y[test_folds == test_fold_id]
+
+    x_train = x[test_folds != test_fold_id, :]
+    y_train = y[test_folds != test_fold_id]
+    return [x_train, y_train, x_test, y_test]
+
+
+# Columns for the DataFrame
+columns=['Dataset', 'MC iteration', 'N-fold id', 'Actual class', 'Model',
+        'AUC', 'Prior']
+# Create a DataFrame to record all intermediate results
+df = pd.DataFrame(columns=columns)
+
 mc_iterations = 10.0
 n_folds = 10.0
-weighted_auc_dens = np.full((len(mldata.datasets), mc_iterations, n_folds), -1)
-weighted_auc_bag = np.full((len(mldata.datasets), mc_iterations, n_folds), -1)
-weighted_auc_com = np.full((len(mldata.datasets), mc_iterations, n_folds), -1)
-
 for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
-    print i
+    print('Dataset number {}'.format(i))
     mldata.sumarize_datasets(name)
-    # if name != "iris": continue
     for mc in np.arange(mc_iterations):
         skf = StratifiedKFold(dataset.target, n_folds=n_folds, shuffle=True)
         test_folds = skf.test_folds
@@ -110,11 +138,14 @@ for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
                 tr_class = x_train[y_train == actual_class, :]
                 t_labels = (y_test == actual_class).astype(int)
                 prior = np.alen(tr_class) / n_training
-                prior_sum += prior
                 if np.alen(tr_class) > 1 and not all(t_labels == 0):
+                    prior_sum += prior
                     n_c = tr_class.shape[1]
                     if n_c > np.alen(tr_class):
                         n_c = np.alen(tr_class)
+
+
+                    # Train a Density estimator
                     #model = GMM(n_components=n_c,
                     #            covariance_type='full')
                     model = GMM(n_components=1, covariance_type='diag')
@@ -125,6 +156,7 @@ for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
 
                     new_data = model.sample(np.alen(tr_class))
 
+                    # Train a Bag of Trees
                     bag = BaggingClassifier(
                         base_estimator=DecisionTreeClassifier(),
                         n_estimators=10)
@@ -135,53 +167,56 @@ for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
 
                     bag.fit(new_data, y)
 
+                    # Combine the results
                     probs = bag.predict_proba(x_test)[:, 1]
                     scores = model.score(x_test)
-                    
-                    com_scores = (probs / np.clip(1.0 - probs,
-                                                  1e-16, 1.0)) * scores
 
+                    com_scores = (probs / np.clip(1.0 - probs,
+                                                  1e-16, 1.0)) * np.exp(scores)
+
+                    # Scores for the Density estimator
                     auc_dens = roc_auc_score(t_labels, scores)
+                    # Scores for the Bag of trees
                     auc_bag = roc_auc_score(t_labels, probs)
+                    # Scores for the Combined model
                     auc_com = roc_auc_score(t_labels, com_scores)
 
-                    w_auc_fold_dens += auc_dens * prior
-                    w_auc_fold_bag += auc_bag * prior
-                    w_auc_fold_com += auc_com * prior
-
-            weighted_auc_dens[i, mc, test_fold] = w_auc_fold_dens / prior_sum
-            weighted_auc_bag[i, mc, test_fold] = w_auc_fold_bag / prior_sum
-            weighted_auc_com[i, mc, test_fold] = w_auc_fold_com / prior_sum
-    w_auc_mean_dens = weighted_auc_dens[i].mean()
-    w_auc_std_dens = weighted_auc_dens[i].std()
-    w_auc_mean_bag = weighted_auc_bag[i].mean()
-    w_auc_std_bag = weighted_auc_bag[i].std()
-    w_auc_mean_com = weighted_auc_com[i].mean()
-    w_auc_std_com = weighted_auc_com[i].std()
-    print ("Weighted AUC density: {} +- {}".format(w_auc_mean_dens,
-                                                   w_auc_std_dens))
-    print ("Weighted AUC Bagged trees: {} +- {}".format(w_auc_mean_bag,
-                                                        w_auc_std_bag))
-    print ("Weighted AUC Combined: {} +- {}".format(w_auc_mean_com,
-                                                        w_auc_std_com))
+                    # Create a new DataFrame to append to the original one
+                    df2 = pd.DataFrame([[name, mc, test_fold, actual_class,
+                                         'Combined', auc_com, prior],
+                                        [name, mc, test_fold, actual_class,
+                                         'P(T$|$X)', auc_bag, prior],
+                                        [name, mc, test_fold, actual_class,
+                                         'P(X$|$A)', auc_dens, prior]],
+                                        columns=columns)
+                    df = df.append(df2, ignore_index=True)
 
 
-print("dataset,size,features,classes,wauc mean dens, wauc std dens,wauc mean "
-      "bag, wauc std bag,wauc mean com, wauc std com,min,max")
-for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
-    w_auc_mean_dens = weighted_auc_dens[i].mean()
-    w_auc_std_dens = weighted_auc_dens[i].std()
-    w_auc_mean_bag = weighted_auc_bag[i].mean()
-    w_auc_std_bag = weighted_auc_bag[i].std()
-    w_auc_mean_com = weighted_auc_com[i].mean()
-    w_auc_std_com = weighted_auc_com[i].std()
-    size = dataset.data.shape[0]
-    n_features = dataset.data.shape[1]
-    classes = len(dataset.classes)
-    minimum = dataset.data.min()
-    maximum = dataset.data.max()
-    if w_auc_mean_dens != -1:
-        print ("{},{},{},{},{},{},{},{},{},{},{},{}".format(
-            name, size, n_features, classes, w_auc_mean_dens, w_auc_std_dens,
-            w_auc_mean_bag, w_auc_std_bag, w_auc_mean_com, w_auc_std_com,
-            minimum, maximum))
+# Convert values to numeric
+df = df.convert_objects(convert_numeric=True)
+
+# Group everything except classes
+dfgroup_classes = df.groupby(by=['Dataset', 'MC iteration', 'N-fold id',
+                                 'Model'])
+# Compute the Prior sum for each dataset, iteration and fold
+df['Prior_sum'] = dfgroup_classes['Prior'].transform(np.sum)
+# Compute the individual weighted AUC per each class and experiment
+df['wAUC'] = df.Prior * df.AUC / df.Prior_sum
+
+# Sum the weighted AUC of each class per each experiment
+series_wAUC = dfgroup_classes['wAUC'].sum()
+
+# Transform the series to a DataFrame
+df_wAUC = series_wAUC.reset_index(inplace=False)
+# Compute mean and standard deviation of wAUC per Dataset and model
+final_results = df_wAUC.groupby(['Dataset', 'Model'])['wAUC'].agg([np.mean,
+    np.std])
+# Transform the series to a DataFrame
+final_results.reset_index(inplace=True)
+
+# Represent the results in a table format
+final_table =  final_results.pivot_table(values=['mean', 'std'],
+                                         index=['Dataset'], columns=['Model'])
+
+# Export the results in a csv and LaTeX file
+export_results(final_table)
