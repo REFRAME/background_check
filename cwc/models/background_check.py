648f8b189ee2c4a64b9200c66542051577191e88
tmfilho <tmfilho@gmail.com>
2016-05-26 15:28:38 +0100

    Changed BackgroundCheck to be able to work with different types of estimators by employing the expit (what an awful name ¬¬) function.

--- cwc/models/background_check.py
+++ cwc/models/background_check.py
@@ -2,6 +2,8 @@ from __future__ import division
 import numpy as np
 from sklearn.mixture import GMM
 from sklearn import datasets
+from scipy.special import expit
+from sklearn.svm import OneClassSVM
 
 
 class BackgroundCheck(object):
@@ -10,8 +12,8 @@ class BackgroundCheck(object):
     Args:
         estimator (object): An object of a class that performs density
             estimation. Any density estimator can be used, as long as it has
-            the following methods: fit(X) and score(X), where X is an array
-            with shape = [n_samples, n_features].
+            the following methods: fit(X) and score(X) or decision_function(X),
+            where X is an array with shape = [n_samples, n_features].
         mu (float): Parameter that sets the deepness of the background
             valleys, compared to foreground peaks.
         m (float): Parameter for how high is the uniform background, compared
@@ -27,6 +29,7 @@ class BackgroundCheck(object):
         self._mu = mu
         self._m = m
         self._max_dens = 0.0
+        self._delta = 0.0
 
     def fit(self, X):
         """Fits the density estimator to the data in X.
@@ -39,7 +42,10 @@ class BackgroundCheck(object):
 
         """
         self._estimator.fit(X)
-        self._max_dens = np.exp(self._estimator.score(X).max())
+        dens = self.score(X)
+        self._delta = 0.0 - dens.min()
+        dens = expit(dens + self._delta)
+        self._max_dens = dens.max()
 
     def predict_proba(self, X):
         """Performs background check on the data in X.
@@ -52,7 +58,7 @@ class BackgroundCheck(object):
             probabilities for background (column 0) and foreground (column 1).
 
         """
-        p_x_f = np.exp(self._estimator.score(X))
+        p_x_f = expit(self.score(X) + self._delta)
         posteriors = np.zeros((np.alen(X), 2))
         q = np.clip(p_x_f / self._max_dens, 0.0, 1.0)
         p_x_and_b = q * self._mu + (1.0 - q) * self._m
@@ -60,17 +66,40 @@ class BackgroundCheck(object):
         posteriors[:, 1] = 1.0 - posteriors[:, 0]
         return posteriors
 
+    def score(self, X):
+        """Gets scores for the objects of X using different functions that
+        depend on the estimator.
+
+        Args:
+            X (array-like, shape = [n_samples, n_features]): training data.
+
+        Returns:
+            (array-like, shape = [n_samples]): scores given by the estimator.
+
+        """
+        if 'decision_function' in dir(self._estimator):
+            return self._estimator.decision_function(X).reshape(-1)
+        elif 'score' in dir(self._estimator):
+            return self._estimator.score(X)
+
 
 if __name__ == "__main__":
     np.random.seed(42)
     dataset = datasets.load_iris()
-    print dataset.data.mean(axis=0)
-    gmm = GMM(n_components=3, covariance_type='diag')
-    bc = BackgroundCheck(estimator=gmm, mu=0.0, m=1.0)
-    bc.fit(dataset.data)
 
     test_indices = np.random.choice(150, 10)
-    test_set = dataset.data#[test_indices, :]
+    test_set = dataset.data[test_indices, :]
     test_set = np.vstack((test_set, np.array([[0.0, 0.0, 0.0, 0.0]])))
     test_set = np.vstack((test_set, np.array([[10.0, 10.0, 10.0, 10.0]])))
-    print bc.predict_proba(test_set)
+
+    print "GMM"
+    gmm = GMM(n_components=3, covariance_type='diag')
+    bc_gmm = BackgroundCheck(estimator=gmm, mu=0.0, m=1.0)
+    bc_gmm.fit(dataset.data)
+    print bc_gmm.predict_proba(test_set)
+
+    print "OneClassSVM"
+    sv = OneClassSVM()
+    bc_sv = BackgroundCheck(estimator=sv, mu=0.0, m=1.0)
+    bc_sv.fit(dataset.data)
+    print bc_sv.predict_proba(test_set)
