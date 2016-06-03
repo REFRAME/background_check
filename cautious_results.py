0d2edc12be23586d909a3b186d6cd3e33b7a04b9
tmfilho <tmfilho@gmail.com>
2016-06-03 17:31:22 +0100

    cautious_results.py now works with multiclass datasets.

--- cautious_results.py
+++ cautious_results.py
@@ -21,21 +21,30 @@ def separate_sets(x, y, test_fold_id, test_folds):
     return [x_train, y_train, x_test, y_test]
 
 
-def accuracy_abstention_curve_Ferri(y, posteriors, ks=np.array([0.5, 0.5]),
-                                n_ws=11):
+def get_abstention_accuracy(y, predictions):
+    n_classes = np.alen(np.unique(y))
+    n_correct = np.sum(predictions == y)
+    n_predicted = np.sum(predictions != n_classes)
+    n_abstained = np.sum(predictions == n_classes)
+    accuracy = n_correct / n_predicted
+    abstention = n_abstained / np.alen(predictions)
+    return abstention, accuracy
+
+
+def accuracy_abstention_curve_ferri(y, posteriors, n_ws=11):
+    n_classes = np.alen(np.unique(y))
+    ks = np.ones(n_classes) / n_classes
     ws = np.linspace(0.0, 1.01, n_ws)
     accuracies = np.zeros(n_ws)
     abstentions = np.zeros(n_ws)
     for index, w in enumerate(ws):
         taus = (1.0 - ks) * w + ks
         predictions = np.argmax(posteriors / taus, axis=1)
-        predictions[posteriors[np.arange(np.alen(predictions)), predictions]
-                    < taus[predictions]] = 2
-        n_correct = np.sum(predictions == y)
-        n_predicted = np.sum(predictions != 2)
-        n_abstained = np.sum(predictions == 2)
-        accuracies[index] = n_correct / n_predicted
-        abstentions[index] = n_abstained / np.alen(predictions)
+        predictions[posteriors[np.arange(np.alen(predictions)),
+                               predictions] < taus[predictions]] = n_classes
+        abstention, accuracy = get_abstention_accuracy(y, predictions)
+        abstentions[index] = abstention
+        accuracies[index] = accuracy
     return abstentions, accuracies
 
 
@@ -51,12 +60,19 @@ def accuracy_abstention_curve_bc(y, posteriors, x_train, x_test, n_mus=11):
         p = np.hstack((posteriors*bc_posteriors[:, 1].reshape(-1, 1),
                        bc_posteriors[:, 0].reshape(-1, 1)))
         predictions = np.argmax(p, axis=1)
-        n_correct = np.sum(predictions == y)
-        n_predicted = np.sum(predictions != 2)
-        n_abstained = np.sum(predictions == 2)
-        accuracies[index] = n_correct / n_predicted
-        abstentions[index] = n_abstained / np.alen(predictions)
+        abstention, accuracy = get_abstention_accuracy(y, predictions)
+        abstentions[index] = abstention
+        accuracies[index] = accuracy
     return abstentions, accuracies
+
+
+def prune_curve(abst, acc):
+    n = np.alen(abst)
+    for i in np.arange(n-1, 0, -1):
+        if acc[i] == acc[i-1]:
+            abst[i] = -1
+            acc[i] = -1
+    return abst[acc > -1], acc[acc > -1]
 
 
 if __name__ == '__main__':
@@ -87,46 +103,42 @@ if __name__ == '__main__':
                 tree.fit(x_train, y_train)
                 posteriors = tree.predict_proba(x_test)
 
-                abst, accs = accuracy_abstention_curve_Ferri(y_test,
+                abst, accs = accuracy_abstention_curve_ferri(y_test,
                                                              posteriors,
                                                              n_ws=n_ws)
 
                 accuracies_ferri[mc * n_folds + test_fold] = accs
                 abstentions_ferri[mc * n_folds + test_fold] = abst
 
-                abst, accs = accuracy_abstention_curve_bc(y_test,
+                abst_bc, accs_bc = accuracy_abstention_curve_bc(y_test,
                                                           posteriors,
                                                           x_train, x_test,
                                                           n_mus=n_ws)
 
-                accuracies_bc[mc * n_folds + test_fold] = accs
-                abstentions_bc[mc * n_folds + test_fold] = abst
+                accuracies_bc[mc * n_folds + test_fold] = accs_bc
+                abstentions_bc[mc * n_folds + test_fold] = abst_bc
 
         # roc = roc_curve(y_test, posteriors, pos_label=0)
-        mean_abst_ferri = abstentions_ferri.mean(axis=0)
-        mean_acc_ferri = accuracies_ferri.mean(axis=0)
+        mean_abst_ferri, mean_acc_ferri = prune_curve(
+            abstentions_ferri.mean(axis=0), accuracies_ferri.mean(axis=0))
         mean_eff_ferri = (mean_acc_ferri - mean_abst_ferri + 1.0) / 2.0
         mean_eff_ferri = mean_eff_ferri[np.logical_not(
             np.isnan(mean_eff_ferri))].mean()
 
-        mean_abst_bc = abstentions_bc.mean(axis=0)
-        mean_acc_bc = accuracies_bc.mean(axis=0)
+        mean_abst_bc, mean_acc_bc = prune_curve(
+            abstentions_bc.mean(axis=0), accuracies_bc.mean(axis=0))
         mean_eff_bc = (mean_acc_bc - mean_abst_bc + 1.0) / 2.0
-        mean_eff_bc = mean_eff_bc[np.logical_not(
-            np.isnan(mean_eff_bc))].mean()
+        mean_eff_bc = mean_eff_bc[np.logical_not(np.isnan(mean_eff_bc))].mean()
 
         fig = plt.figure("Background Check")
         fig.clf()
         ax = fig.add_subplot(111)
         ax.plot(mean_abst_ferri, mean_acc_ferri, 'b.-',
-                label="Ferri (mean efficacy = {0:.2f})".format(mean_eff_ferri) )
+                label="Ferri (mean efficacy = {0:.2f})".format(mean_eff_ferri))
         ax.plot(mean_abst_bc, mean_acc_bc, 'r.--',
                 label="BC (mean efficacy = {0:.2f})".format(mean_eff_bc))
         ax.set_xlabel('Abstention')
         ax.set_ylabel('Accuracy')
         ax.legend(loc='lower right')
-        # ax.set_xlim([-0.01, 1.01])
-        # ax.set_ylim([-0.01, 1.01])
-        #plt.show()
 
         plt.show()
