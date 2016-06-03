f24c09b50e2825c133882d1c52026ee9253b5715
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-06-03 15:07:26 +0100

    Removed code for Weka

--- cautious_results.py
+++ cautious_results.py
@@ -1,13 +1,6 @@
 from __future__ import division
 import numpy as np
 
-from weka.core.classes import from_commandline
-from weka.core.converters import ndarray_to_instances
-from weka.classifiers import FilteredClassifier
-from weka.filters import Filter
-import traceback
-import weka.core.jvm as jvm
-
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.svm import OneClassSVM
 from sklearn.mixture import GMM
@@ -26,40 +19,6 @@ def separate_sets(x, y, test_fold_id, test_folds):
     x_train = x[test_folds != test_fold_id, :]
     y_train = y[test_folds != test_fold_id]
     return [x_train, y_train, x_test, y_test]
-
-
-def get_weka_instances(data, name):
-    weka_instances = ndarray_to_instances(data, name)
-    weka_instances.class_is_last()
-    return weka_instances
-
-
-def weka_tree():
-    tree = FilteredClassifier()
-    cmdline = 'weka.classifiers.trees.J48 -U -A'
-    tree.classifier = from_commandline(cmdline,
-                                       classname="weka.classifiers.Classifier")
-    flter = Filter(
-        "weka.filters.unsupervised.attribute.NumericToNominal")
-    flter.options = ["-R", "last"]
-    tree.filter = flter
-    return tree
-
-
-def fit(tree, X, y):
-    training_set = get_weka_instances(np.hstack((X, y.reshape(-1, 1))), '')
-    tree.build_classifier(training_set)
-
-
-def predict_proba(tree, X, y):
-    n = np.alen(X)
-    n_classes = np.alen(np.unique(y))
-    test_set = get_weka_instances(np.hstack((X, y.reshape(-1, 1))), '')
-    posteriors = np.zeros((n, n_classes))
-    for instance in np.arange(n):
-        posteriors[instance] = tree.distribution_for_instance(
-            test_set.get_instance(instance))
-    return posteriors
 
 
 def accuracy_abstention_curve_Ferri(y, posteriors, ks=np.array([0.5, 0.5]),
@@ -107,77 +66,67 @@ if __name__ == '__main__':
     mc_iterations = 20
     n_folds = 5
     n_ws = 100
-    try:
-        #jvm.start()
-        for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
-            if name != 'tic-tac':
-                continue
-            accuracies_ferri = np.zeros((mc_iterations * n_folds, n_ws))
-            abstentions_ferri = np.zeros((mc_iterations * n_folds, n_ws))
-            accuracies_bc = np.zeros((mc_iterations * n_folds, n_ws))
-            abstentions_bc = np.zeros((mc_iterations * n_folds, n_ws))
-            mldata.sumarize_datasets(name)
-            for mc in np.arange(mc_iterations):
-                skf = StratifiedKFold(dataset.target, n_folds=n_folds,
-                                      shuffle=True)
-                test_folds = skf.test_folds
-                for test_fold in np.arange(n_folds):
-                    x_train, y_train, x_test, y_test = separate_sets(
-                            dataset.data, dataset.target, test_fold, test_folds)
+    for i, (name, dataset) in enumerate(mldata.datasets.iteritems()):
+        if name != 'tic-tac':
+            continue
+        accuracies_ferri = np.zeros((mc_iterations * n_folds, n_ws))
+        abstentions_ferri = np.zeros((mc_iterations * n_folds, n_ws))
+        accuracies_bc = np.zeros((mc_iterations * n_folds, n_ws))
+        abstentions_bc = np.zeros((mc_iterations * n_folds, n_ws))
+        mldata.sumarize_datasets(name)
+        for mc in np.arange(mc_iterations):
+            skf = StratifiedKFold(dataset.target, n_folds=n_folds,
+                                  shuffle=True)
+            test_folds = skf.test_folds
+            for test_fold in np.arange(n_folds):
+                x_train, y_train, x_test, y_test = separate_sets(
+                        dataset.data, dataset.target, test_fold, test_folds)
 
 
-                    #tree = weka_tree()
-                    #fit(tree, x_train, y_train)
-                    #posteriors = predict_proba(tree, x_test, y_test)
-                    tree = MyDecisionTreeClassifier(min_samples_leaf=2)
-                    tree.fit(x_train, y_train)
-                    posteriors = tree.predict_proba(x_test)
+                tree = MyDecisionTreeClassifier(min_samples_leaf=2)
+                tree.fit(x_train, y_train)
+                posteriors = tree.predict_proba(x_test)
 
-                    abst, accs = accuracy_abstention_curve_Ferri(y_test,
-                                                                 posteriors,
-                                                                 n_ws=n_ws)
+                abst, accs = accuracy_abstention_curve_Ferri(y_test,
+                                                             posteriors,
+                                                             n_ws=n_ws)
 
-                    accuracies_ferri[mc * n_folds + test_fold] = accs
-                    abstentions_ferri[mc * n_folds + test_fold] = abst
+                accuracies_ferri[mc * n_folds + test_fold] = accs
+                abstentions_ferri[mc * n_folds + test_fold] = abst
 
-                    abst, accs = accuracy_abstention_curve_bc(y_test,
-                                                              posteriors,
-                                                              x_train, x_test,
-                                                              n_mus=n_ws)
+                abst, accs = accuracy_abstention_curve_bc(y_test,
+                                                          posteriors,
+                                                          x_train, x_test,
+                                                          n_mus=n_ws)
 
-                    accuracies_bc[mc * n_folds + test_fold] = accs
-                    abstentions_bc[mc * n_folds + test_fold] = abst
+                accuracies_bc[mc * n_folds + test_fold] = accs
+                abstentions_bc[mc * n_folds + test_fold] = abst
 
-            # roc = roc_curve(y_test, posteriors, pos_label=0)
-            mean_abst_ferri = abstentions_ferri.mean(axis=0)
-            mean_acc_ferri = accuracies_ferri.mean(axis=0)
-            mean_eff_ferri = (mean_acc_ferri - mean_abst_ferri + 1.0) / 2.0
-            mean_eff_ferri = mean_eff_ferri[np.logical_not(
-                np.isnan(mean_eff_ferri))].mean()
+        # roc = roc_curve(y_test, posteriors, pos_label=0)
+        mean_abst_ferri = abstentions_ferri.mean(axis=0)
+        mean_acc_ferri = accuracies_ferri.mean(axis=0)
+        mean_eff_ferri = (mean_acc_ferri - mean_abst_ferri + 1.0) / 2.0
+        mean_eff_ferri = mean_eff_ferri[np.logical_not(
+            np.isnan(mean_eff_ferri))].mean()
 
-            mean_abst_bc = abstentions_bc.mean(axis=0)
-            mean_acc_bc = accuracies_bc.mean(axis=0)
-            mean_eff_bc = (mean_acc_bc - mean_abst_bc + 1.0) / 2.0
-            mean_eff_bc = mean_eff_bc[np.logical_not(
-                np.isnan(mean_eff_bc))].mean()
+        mean_abst_bc = abstentions_bc.mean(axis=0)
+        mean_acc_bc = accuracies_bc.mean(axis=0)
+        mean_eff_bc = (mean_acc_bc - mean_abst_bc + 1.0) / 2.0
+        mean_eff_bc = mean_eff_bc[np.logical_not(
+            np.isnan(mean_eff_bc))].mean()
 
-            fig = plt.figure("Background Check")
-            fig.clf()
-            ax = fig.add_subplot(111)
-            ax.plot(mean_abst_ferri, mean_acc_ferri, 'b.-',
-                    label="Ferri (mean efficacy = {0:.2f})".format(mean_eff_ferri) )
-            ax.plot(mean_abst_bc, mean_acc_bc, 'r.--',
-                    label="BC (mean efficacy = {0:.2f})".format(mean_eff_bc))
-            ax.set_xlabel('Abstention')
-            ax.set_ylabel('Accuracy')
-            ax.legend(loc='lower right')
-            # ax.set_xlim([-0.01, 1.01])
-            # ax.set_ylim([-0.01, 1.01])
-            #plt.show()
+        fig = plt.figure("Background Check")
+        fig.clf()
+        ax = fig.add_subplot(111)
+        ax.plot(mean_abst_ferri, mean_acc_ferri, 'b.-',
+                label="Ferri (mean efficacy = {0:.2f})".format(mean_eff_ferri) )
+        ax.plot(mean_abst_bc, mean_acc_bc, 'r.--',
+                label="BC (mean efficacy = {0:.2f})".format(mean_eff_bc))
+        ax.set_xlabel('Abstention')
+        ax.set_ylabel('Accuracy')
+        ax.legend(loc='lower right')
+        # ax.set_xlim([-0.01, 1.01])
+        # ax.set_ylim([-0.01, 1.01])
+        #plt.show()
 
-            plt.show()
-    except Exception, e:
-        print(traceback.format_exc())
-    #finally:
-        #jvm.stop()
-
+        plt.show()
