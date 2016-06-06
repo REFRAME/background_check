2eb2bc0179076284cc9163d400ed74f9d2b0f463
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-06-06 21:58:33 +0100

    Added test for datasets in article Li2014

--- cwc/synthetic_data/datasets.py
+++ cwc/synthetic_data/datasets.py
@@ -409,7 +409,7 @@ class Data(object):
             for name, dataset in self.datasets.iteritems():
                 dataset.print_summary()
 
-def test(dataset_names):
+def test_datasets(dataset_names):
     from sklearn.svm import SVC
     from sklearn.cross_validation import StratifiedKFold
     data = Data(dataset_names=dataset_names)
@@ -423,11 +423,12 @@ def test(dataset_names):
         return [x_train, y_train, x_test, y_test]
 
     n_folds = 2
+    accuracies = {}
     for name, dataset in data.datasets.iteritems():
         dataset.print_summary()
         skf = StratifiedKFold(dataset.target, n_folds=n_folds, shuffle=True)
         test_folds = skf.test_folds
-        accuracies = np.zeros(n_folds)
+        accuracies[name] = np.zeros(n_folds)
         for test_fold in np.arange(n_folds):
             x_train, y_train, x_test, y_test = separate_sets(
                     dataset.data, dataset.target, test_fold, test_folds)
@@ -435,10 +436,26 @@ def test(dataset_names):
             svc = SVC(C=1.0, kernel='rbf', degree=1, tol=0.01)
             svc.fit(x_train, y_train)
             prediction = svc.predict(x_test)
-            accuracies[test_fold] = 100*np.mean((prediction == y_test))
-        print("Accuracy = {0:.2f}% +- {1:.2f}".format(accuracies.mean(),
-                                                      accuracies.std()))
-        print('')
+            accuracies[name][test_fold] = 100*np.mean((prediction == y_test))
+            print("Acc = {0:.2f}%".format(accuracies[name][test_fold]))
+    return accuracies
+
+def test():
+    dataset_names = ['autos', 'car', 'cleveland', 'dermatology', 'ecoli',
+            'flare', 'glass', 'led7digit', 'lymphography', 'nursery',
+            'page-blocks', 'pendigits', 'satimage', 'segment', 'shuttle',
+            'vehicle', 'vowel', 'yeast', 'zoo', 'auslan']
+
+    not_available_yet = ['satimage', 'nursery', 'lymphography', 'auslan',
+                         'led7digit', 'yeast']
+
+    valid_dataset_names = [name for name in dataset_names if name not in not_available_yet]
+
+    accuracies = test_datasets(valid_dataset_names)
+    for name in valid_dataset_names:
+        print("{}. {} Acc = {:.2f}% +- {:.2f}".format(
+                np.where(np.array(dataset_names) == name)[0]+1,
+                name, accuracies[name].mean(), accuracies[name].std()))
 
 if __name__=='__main__':
-    test(['zoo'])
+    test()
