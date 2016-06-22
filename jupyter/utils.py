44174dd874af31764ad31e44ff659bea21048c53
Miquel Perelló Nieto <perello.nieto@gmail.com>
2016-06-22 20:39:42 +0200

    Added useful functions for jupyter tutorials

--- jupyter/utils.py
+++ jupyter/utils.py
@@ -1,13 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
+from scipy.stats import norm
 
 def draw_in_row(fruits, sizes):
-    images = map(Image.open, ['./images/'+fruit+'.jpg' for fruit in fruits])
+    indices = np.argsort(sizes)
+    sizes_sorted = sizes[indices]
+    fruits_sorted = fruits[indices]
+
+    images = map(Image.open, ['./images/'+fruit+'.jpg' for fruit in
+                              fruits_sorted])
     widths, heights = zip(*(i.size for i in images))
 
     unit_width = 200
-    drawing_sizes = np.array(sizes*unit_width, dtype=int)
+
+    drawing_sizes = np.array(sizes_sorted*unit_width, dtype=int)
     total_width = drawing_sizes.sum()+unit_width
     max_height = drawing_sizes.max()
 
@@ -29,6 +36,40 @@ def draw_in_row(fruits, sizes):
     ax = plt.figure(figsize=(7,5), dpi=80)
     plt.imshow(new_im)
     plt.axis('off')
+
+
+class NormalDistribution(object):
+    def __init__(self, x=None, mu=None, sigma=None):
+        if x is not None:
+            self.fit(x)
+        else:
+            if mu is not None:
+                self.mu = mu
+            if sigma is not None:
+                self.sigma = sigma
+
+    def fit(self, x):
+        self.mu = x.mean()
+        self.sigma = x.std()
+
+    def pdf(self,x):
+        return norm.pdf(x, loc=self.mu, scale=self.sigma)
+
+    def sample(self, n):
+        return norm.rvs(loc=self.mu, scale=self.sigma, size=n)
+
+
+def plot_confusion_matrix(cm, labels, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(labels))
+    plt.xticks(tick_marks, labels, rotation=45)
+    plt.yticks(tick_marks, labels)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
 
 if __name__ == '__main__':
     pass
